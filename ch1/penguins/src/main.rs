453122bdc7c99a249cee475cd312e88036c7dc2d
pedro_tentugal <pedrotentugal@gmail.com>
2026-01-18 19:20:30 +0000

    Updated to single repo for the first and second chapter, since they're just intro to language. Will see if I split it on the next chapters.

---
+++ ch1/penguins/src/main.rs
@@ -0,0 +1,43 @@
+fn main() {
+    penguin();
+}
+
+fn penguin() {
+    let penguin_ai_drawing = r"
+        .--.
+       |o_o |
+       |:_/ |
+      //   \\ \\
+     (|     | )
+    /'\\_   _/`\\
+    \\___)=(___/";
+
+    println!("{}", penguin_ai_drawing);
+
+    let penguin_data = "\
+    common name,length (cm)
+    Little penguin,33
+    Yellow-eyed penguin,65
+    Fiordland penguin,60
+    Invalid,data
+    ";
+
+    let records = penguin_data.lines();
+    for (i, record) in records.enumerate() {
+        if i == 0 || record.trim().len() == 0 {
+            continue;
+        }
+        let fields: Vec<_> = record
+            .split(",")
+            .map(|x| x.trim())
+            .collect();
+        if cfg!(debug_assertions) {
+            eprintln!("debug: {:?} -> {:?}", record, fields);
+        }
+
+        let name = fields[0];
+        if let Ok(length) = fields[1].parse::<f32>() {
+            println!("{}, {}cm", name, length);
+        }
+    }
+}
