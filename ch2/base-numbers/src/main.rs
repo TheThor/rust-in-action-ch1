64ca849a32fd8e0a367b76d13ea9f2971c8a7151
pedro_tentugal <pedrotentugal@gmail.com>
2026-01-20 23:33:52 +0000

    Adding base numbers definition.

--- ch2/base-numbers/src/main.rs
+++ ch2/base-numbers/src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
     let three = 0b11; // Mean binary 11 is equal to 3
-    let one = 0b01; // Mean binary 01 is equal to 1
+    let _one = 0b01; // Mean binary 01 is equal to 1
     let thirty = 0o36; // Octo base representation of 30
     let three_hundred = 0x12C;
+
+    println!("base 10: {} {} {}", three, thirty, three_hundred);
+    println!("base 2:  {:b} {:b} {:b}", three, thirty, three_hundred);
+    println!("base 8:  {:o} {:o} {:o}", three, thirty, three_hundred);
+    println!("base 16: {:x} {:x} {:x}", three, thirty, three_hundred);
 }
